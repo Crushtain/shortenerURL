767d5e6ab01f017ecd1e7b478ae566db2774fe6b
Crushtain <vova.muzylev@mail.ru>
2023-08-11 18:54:42 +0300

    дополнил тесты для handlers

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,68 +1,90 @@
 package handlers
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/Crushtain/shortenerURL/storage"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 )
 
-func TestShortenHandler(t *testing.T) {
-	// Создаем новый тестовый запрос
-	body := []byte("example-url")
-	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Создаем тестовый ResponseWriter для записи ответа
-	recorder := httptest.NewRecorder()
-
-	// Вызываем хендлер
-	h := &URLHandler{}
-	h.Shorten(recorder, req)
-
-	// Проверяем статус код ответа
-	if recorder.Code != http.StatusCreated {
-		t.Errorf("Ожидался статус код %d, получен %d",
-			http.StatusCreated, recorder.Code)
-	}
-
-	// Проверяем тип данных контента
-	contentType := recorder.Header().Get("Content-Type")
-	if contentType != "text/plain" {
-		t.Errorf("Ожидался тип данных 'text/plain', получен '%s'",
-			contentType)
-	}
-
-	// Проверяем тело ответа
-	responseBody, err := io.ReadAll(recorder.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedResponse := "http://localhost:8080/" + string(body)
-	actualResponse := strings.TrimSpace(string(responseBody))
-	if expectedResponse != actualResponse {
-		t.Errorf("Ожидался ответ '%s', получен '%s'",
-			expectedResponse, actualResponse)
-	}
-}
-
 type mockStorage struct {
 	data     map[string]string
+	mu       sync.RWMutex
 	InMemory *storage.InMemory
 }
 
 func (m *mockStorage) Put(short, body string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.data[short] = body
 }
 
 func (m *mockStorage) Get(short string) string {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	return m.data[short]
+}
+
+func TestShortenHandler(t *testing.T) {
+	// Создаем экземпляр URLHandler с использованием mockStorage
+	storage := &mockStorage{
+		data: make(map[string]string),
+	}
+	urlHandler := &URLHandler{
+		storage: storage.InMemory,
+	}
+
+	// Создаем тестовый HTTP-запрос с телом запроса
+	reqBody := strings.NewReader("http://example.com")
+	req, err := http.NewRequest("POST", "/", reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	// Вызываем обработчик Shorten
+	handler := http.HandlerFunc(urlHandler.Shorten)
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("Ожидаемый статус-код %v, получен %v", http.StatusCreated, status)
+	}
+
+	// Проверяем заголовок Content-type
+	expectedContentType := "text/plain"
+	contentType := rr.Header().Get("Content-type")
+	if contentType != expectedContentType {
+		t.Errorf("Ожидаемый заголовок Content-type %q, получен %q", expectedContentType, contentType)
+	}
+
+	// Проверяем вывод в теле ответа
+	expectedOutput := "http://localhost:8080/encoded_short"
+	actualOutput := fmt.Sprint(rr.Body)
+	if actualOutput != expectedOutput {
+		t.Errorf("Ожидаемый вывод %q, получен %q", expectedOutput, actualOutput)
+	}
+
+	// Создаем тестовый HTTP-запрос без тела запроса (неверный запрос)
+	req, err = http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr = httptest.NewRecorder()
+
+	// Вызываем обработчик Shorten
+	handler = http.HandlerFunc(urlHandler.Shorten)
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Ожидаемый статус-код %v, получен %v", http.StatusBadRequest, status)
+	}
 }
 
 func TestOriginalHandler(t *testing.T) {
@@ -74,8 +96,11 @@ func TestOriginalHandler(t *testing.T) {
 		storage: storage.InMemory,
 	}
 
+	// Добавляем данные в хранилище
+	storage.Put("encoded_short", "http://example.com")
+
 	// Создаем тестовый HTTP-запрос с корректным шорткодом
-	req, err := http.NewRequest("GET", "/abc123", nil)
+	req, err := http.NewRequest("GET", "/encoded_short", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -88,21 +113,21 @@ func TestOriginalHandler(t *testing.T) {
 
 	// Проверяем статус-код
 	if status := rr.Code; status != http.StatusTemporaryRedirect {
-		t.Errorf("Expected status code %v but got %v", http.StatusTemporaryRedirect, status)
+		t.Errorf("Ожидаемый статус-код %v, получен %v", http.StatusTemporaryRedirect, status)
 	}
 
 	// Проверяем заголовок Location
-	expectedLocation := "value"
+	expectedLocation := "http://example.com"
 	location := rr.Header().Get("Location")
 	if location != expectedLocation {
-		t.Errorf("Expected Location header %q but got %q", expectedLocation, location)
+		t.Errorf("Ожидаемый заголовок Location %q, получен %q", expectedLocation, location)
 	}
 
 	// Проверяем вывод в консоль
-	expectedOutput := "value"
+	expectedOutput := "http://example.com"
 	actualOutput := fmt.Sprint(rr.Body)
 	if actualOutput != expectedOutput {
-		t.Errorf("Expected output %q but got %q", expectedOutput, actualOutput)
+		t.Errorf("Ожидаемый вывод %q, получен %q", expectedOutput, actualOutput)
 	}
 
 	// Создаем тестовый HTTP-запрос с некорректным шорткодом
@@ -113,12 +138,12 @@ func TestOriginalHandler(t *testing.T) {
 
 	rr = httptest.NewRecorder()
 
-	// Вызываем обработчик Original
+	// Вызываем об работчик Original
 	handler = http.HandlerFunc(urlHandler.Original)
 	handler.ServeHTTP(rr, req)
 
 	// Проверяем статус-код
 	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("Expected status code %v but got %v", http.StatusBadRequest, status)
+		t.Errorf("Ожидаемый статус-код %v, получен %v", http.StatusBadRequest, status)
 	}
 }
