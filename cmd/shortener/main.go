dbeff5160e09d2c536495c7a2682d1b109230bf2
Crushtain <vova.muzylev@mail.ru>
2023-08-13 15:40:05 +0300

    добавил флаги

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"github.com/Crushtain/shortenerURL/config"
 	"github.com/Crushtain/shortenerURL/handlers"
 	"github.com/Crushtain/shortenerURL/storage"
 	"github.com/go-chi/chi/v5"
@@ -8,14 +10,17 @@ import (
 )
 
 func main() {
+	conf := config.New()
+	flag.Parse()
+
 	memory := storage.NewInMemory()
-	urlHandler := handlers.NewURL(memory)
+	urlHandler := handlers.NewURL(memory, conf)
 
 	router := chi.NewRouter()
 	router.Post("/", urlHandler.Shorten)
 	router.Get("/{id}", urlHandler.Original)
 
-	err := http.ListenAndServe("localhost:8080", router)
+	err := http.ListenAndServe(conf.Host, router)
 	if err != nil {
 		panic(err)
 	}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/Crushtain/shortenerURL/config"
 	storage "github.com/Crushtain/shortenerURL/storage"
 	"github.com/jxskiss/base62"
 	"io"
@@ -13,10 +14,11 @@ import (
 type URLHandler struct {
 	mu      sync.RWMutex
 	storage *storage.InMemory
+	url     *config.Config
 }
 
-func NewURL(store *storage.InMemory) *URLHandler {
-	return &URLHandler{storage: store}
+func NewURL(store *storage.InMemory, config *config.Config) *URLHandler {
+	return &URLHandler{storage: store, url: config}
 }
 
 func (h *URLHandler) Save(short string, body string) {
@@ -35,9 +37,15 @@ func (h *URLHandler) Shorten(w http.ResponseWriter, r *http.Request) {
 	short := base62.Encode(body)        //кодирую
 	h.Save(string(short), string(body)) //сохраняю
 	out := "http://localhost:8080/" + string(short)
+	//flag.StringVar("b", out, "fgwewe")
+	response := fmt.Sprintf(h.url.ResultURL+"/%s", short)
 	w.Header().Set("Content-type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprint(w, string(out))
+	_, err = io.WriteString(w, response)
+	if err != nil {
+		return
+	}
 
 }
 
