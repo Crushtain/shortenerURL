5a69dc3415aa28f76031f20feba052256db407d7
Crushtain <vova.muzylev@mail.ru>
2023-08-18 14:59:29 +0300

    исправил замечания

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -43,7 +43,7 @@ func (c *Config) Shorten(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
 
-	io.WriteString(w, response)
+	_, _ = io.WriteString(w, response)
 
 }
 
